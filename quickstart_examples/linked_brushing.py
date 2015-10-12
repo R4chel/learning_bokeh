e7b1a8af256db2971daae64c7dcf6c51d64db976
Rachel Ehrlich <rae84@cornell.edu>
2015-10-12 13:45:17 -0400

    add linked brushing example

---
+++ quickstart_examples/linked_brushing.py
@@ -0,0 +1,27 @@
+import numpy as np
+from bokeh.plotting import *
+from bokeh.models import ColumnDataSource
+
+# prep data
+N = 300
+x = np.linspace(0, 4*np.pi, N)
+y0 = np.sin(x)
+y1 = np.cos(x)
+
+# output file
+output_file("linked_brushing.html")
+
+source = ColumnDataSource(data=dict(x=x,y0=y0,y1=y1))
+
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
+
+# create new plot and renderer
+left = figure(tools=TOOLS, width=350, height=350)
+left.circle('x','y0',source=source)
+
+right = figure(tools=TOOLS, width=350, height=350)
+right.circle('x','y1',source=source)
+
+p = gridplot([[left,right]])
+
+show(p)
